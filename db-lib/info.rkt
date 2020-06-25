09b268d16e6ebdd8bee051c2c3be8b1cd1c2929e
Ryan Culpepper <ryanc@racket-lang.org>
2020-06-25 14:41:12 +0200

    update version number to 1.7

--- db-lib/info.rkt
+++ db-lib/info.rkt
@@ -1,10 +1,10 @@
 #lang setup/infotab
 
-(define version "1.6")
+(define version "1.7")
 
 (define collection 'multi)
 (define deps '("srfi-lite-lib"
-               ["base" #:version "7.0.0.5"]
+               ["base" #:version "7.7.0.9"]
                "unix-socket-lib"
                ["sasl-lib" #:version "1.1"]))
 
